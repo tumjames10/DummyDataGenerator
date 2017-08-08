a09686e5c9b0f0a661d056426664f58efead8b21
Tumulak <billy.j.b.tumulak@accenture.com>
2017-08-08 18:55:06 +0800

    support nullable type

--- DummyDataGenerator/Dummy.cs
+++ DummyDataGenerator/Dummy.cs
@@ -11,7 +11,6 @@ namespace Dummy
     {
         private Random random;
 
-
         // set of capital Characters.
         private int[] capitalLetters = new int[] { 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90 };
 
